3c3c7b7259f8e714758a7b92a8e1aeacc5f4f341
shashwat_manglam_jain <135765521+Shashwat-Manglam-Jain@users.noreply.github.com>
2025-11-11 06:03:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -242,4 +242,5 @@ def analyze_prescription():
 #  Run the App
 # ===========================
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # use Render's port if available
+    app.run(host="0.0.0.0", port=port)
